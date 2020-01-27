9942336c9434af941139f5c13b19281cb995b1b2
Michel <michell@corp.saic.com>
2020-01-27 12:44:26 -0600

    Added .env file

--- server/src/main/java/Config.java
+++ server/src/main/java/Config.java
@@ -1,23 +1,11 @@
-import java.net.URL;
-import java.util.Properties;
-
 public class Config
 {
-    Properties configFile;
     public Config()
-    {
-        configFile = new java.util.Properties();
-        try {
-            URL url = getClass().getResource("config.cfg");
-            configFile.load(url.openStream());
-        }catch(Exception eta){
-            eta.printStackTrace();
-        }
-    }
+    { }
 
     public String getProperty(String key)
     {
-        String value = this.configFile.getProperty(key);
+        String value = System.getenv(key);
         return value;
     }
 }
