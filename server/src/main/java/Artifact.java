0c0fdca9335fd9ea99d92d949dd07c0d12c25632
Michel <michell@corp.saic.com>
2020-01-28 15:51:24 -0600

    config change

--- server/src/main/java/Artifact.java
+++ server/src/main/java/Artifact.java
@@ -22,4 +22,26 @@ public class Artifact {
     void addLink(Link link){
         this.links.add(link);
     }
+
+    // Overriding equals() to compare two Complex objects
+    @Override
+    public boolean equals(Object o) {
+
+        // If the object is compared with itself then return true
+        if (o == this) {
+            return true;
+        }
+
+        /* Check if o is an instance of Complex or not
+          "null instanceof [type]" also returns false */
+        if (!(o instanceof Artifact)) {
+            return false;
+        }
+
+        // typecast o to Complex so that we can compare data members
+        Artifact c = (Artifact) o;
+
+        // Compare the data members and return accordingly
+        return ((c.artifactType.equals(this.artifactType)) && (c.itemId.equals(this.itemId)) && (c.id.equals(this.id)) && (c.name.equals(this.name)) && (c.url.equals(this.url)) && (c.parentFolder.equals(this.parentFolder)));
+    }
 }
