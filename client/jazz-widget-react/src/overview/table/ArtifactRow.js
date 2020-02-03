33fe3d1f52b15faacbcf1020070cb497b9100843
Michel <michell@corp.saic.com>
2020-02-03 10:55:23 -0600

    Jazz widget updates

--- client/jazz-widget-react/src/overview/table/ArtifactRow.js
+++ client/jazz-widget-react/src/overview/table/ArtifactRow.js
@@ -2,6 +2,7 @@ import React from "react";
 import { slideDown, slideUp } from "./anim";
 import "./style.css";
 import LinkRow from "./LinkRow";
+import { Accordion, Table } from "react-bootstrap";
 
 const isSearched = searchTerm => item =>
   item.artifactName.toLowerCase().includes(searchTerm.toLowerCase());
@@ -29,35 +30,35 @@ class ArtifactRow extends React.Component {
 
   render() {
     const { folder, searchTerm } = this.props;
-    return [
-      <tr key="main" onClick={this.toggleExpander}>
-        <td className="uk-text-nowrap">{folder.folderName}</td>
-        <td>{folder.numArtifacts}</td>
-      </tr>,
-      this.state.expanded && !folder.numArtifacts < 1 && (
-        <tr className="expandable" key="tr-expander">
-          <div ref="expanderBody" className="inner uk-grid">
-            <div className="uk-text-center">
-              <table>
-                <thead>
-                  <th>ID</th>
-                  <th>Name</th>
-                  <th>Type</th>
-                  <th>Number of Links</th>
-                </thead>
-                <tbody>
-                  {folder.artifacts
-                    .filter(isSearched(searchTerm))
-                    .map(artifact => (
-                      <LinkRow artifact={artifact} />
-                    ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </tr>
-      )
-    ];
+    return (
+      <div>
+        <Accordion.Toggle as={Button} eventKey="0">
+          <tr key="main">
+            <td className="uk-text-nowrap">{folder.folderName}</td>
+            <td>{folder.numArtifacts}</td>
+          </tr>
+        </Accordion.Toggle>
+        {!folder.numArtifacts < 1 && (
+          <Accordion.Collapse eventKey="0">
+            <Table striped bordered hover>
+              <thead>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Number of Links</th>
+              </thead>
+              <tbody>
+                {folder.artifacts
+                  .filter(isSearched(searchTerm))
+                  .map(artifact => (
+                    <LinkRow artifact={artifact} />
+                  ))}
+              </tbody>
+            </Table>
+          </Accordion.Collapse>
+        )}
+      </div>
+    );
   }
 }
 
