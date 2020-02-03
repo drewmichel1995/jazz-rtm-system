d3783eb7756754dce3a365f05cc54b38b4ad7b6c
Michel <michell@corp.saic.com>
2020-02-03 13:00:05 -0600

    Jazz widget updates

--- client/jazz-widget-react/src/overview/table/LinkRow.js
+++ client/jazz-widget-react/src/overview/table/LinkRow.js
@@ -1,64 +1,44 @@
 import React from "react";
-import { slideDown, slideUp } from "./anim";
 import "./style.css";
+import { Accordion, Table } from "react-bootstrap";
 
 class LinkRow extends React.Component {
-  state = { expanded: false };
-
-  toggleExpander = e => {
-    if (e.target.type === "checkbox") return;
-
-    if (!this.state.expanded) {
-      this.setState({ expanded: true }, () => {
-        if (this.refs.expanderBody) {
-          slideDown(this.refs.expanderBody);
-        }
-      });
-    } else {
-      slideUp(this.refs.expanderBody, {
-        onComplete: () => {
-          this.setState({ expanded: false });
-        }
-      });
-    }
-  };
-
   render() {
     const { artifact } = this.props;
-    return [
-      <tr key="main" onClick={this.toggleExpander}>
-        <td>{artifact.artifactID}</td>
+    return (
+      <tr key="main">
+        <td>
+          <Accordion.Toggle as="div" eventKey="1">
+            {artifact.artifactID}
+          </Accordion.Toggle>
+        </td>
         <td>{artifact.artifactName}</td>
         <td>{artifact.artifactType}</td>
         <td>{artifact.numLinks}</td>
-      </tr>,
-      this.state.expanded && !artifact.numLinks < 1 && (
-        <tr className="expandable" key="tr-expander">
-          <div ref="expanderBody" className="inner uk-grid">
-            <div className="uk-text-center">
-              <table>
-                <thead>
-                  <th>ID</th>
-                  <th>Name</th>
-                  <th>Link Type</th>
-                  <th>Link Category</th>
-                </thead>
-                <tbody>
-                  {artifact.links.map(link => (
-                    <tr>
-                      <td>{link.id}</td>
-                      <td>{link.linkName}</td>
-                      <td>{link.linkType}</td>
-                      <td>{link.category}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </tr>
-      )
-    ];
+        {!artifact.numLinks < 1 && (
+          <Accordion.Collapse eventKey="1">
+            <Table striped bordered hover>
+              <thead>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Link Type</th>
+                <th>Link Category</th>
+              </thead>
+              <tbody>
+                {artifact.links.map(link => (
+                  <tr>
+                    <td>{link.id}</td>
+                    <td>{link.linkName}</td>
+                    <td>{link.linkType}</td>
+                    <td>{link.category}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Accordion.Collapse>
+        )}
+      </tr>
+    );
   }
 }
 
