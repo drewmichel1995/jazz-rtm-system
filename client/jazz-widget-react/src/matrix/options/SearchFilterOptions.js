35981b676c693463fea0c994c053f176dfd736ec
Michel <michell@corp.saic.com>
2020-02-28 08:14:07 -0600

    usability and responsive changes

--- client/jazz-widget-react/src/matrix/options/SearchFilterOptions.js
+++ client/jazz-widget-react/src/matrix/options/SearchFilterOptions.js
@@ -6,32 +6,28 @@ class SearchFilterOptions extends Component {
     const { onRowChange, onColumnChange, triggerShowID, showID } = this.props;
     return (
       <Form inline>
-        <Form.Row>
-          <Col>
-            <Form.Check
-              type="switch"
-              id="show-id-switch"
-              onChange={triggerShowID}
-              checked={showID}
-              className="form-text"
-              label="Show ID"
-            />
-          </Col>
-          <Col>
-            <Form.Control
-              onChange={onColumnChange}
-              placeholder="Search Columns"
-              className="search-box"
-            />
-          </Col>
-          <Col>
-            <Form.Control
-              onChange={onRowChange}
-              placeholder="Search Rows"
-              className="search-box"
-            />
-          </Col>
-        </Form.Row>
+        <Form.Group>
+          <Form.Check
+            type="switch"
+            id="show-id-switch"
+            onChange={triggerShowID}
+            checked={showID}
+            className="form-text"
+            label="Show ID"
+          />
+
+          <Form.Control
+            onChange={onColumnChange}
+            placeholder="Search Columns"
+            className="search-box"
+          />
+
+          <Form.Control
+            onChange={onRowChange}
+            placeholder="Search Rows"
+            className="search-box"
+          />
+        </Form.Group>
       </Form>
     );
   }
