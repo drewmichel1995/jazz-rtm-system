56d74dd3b6fd85cea5dcb5d57a7f37f85f54e631
Drew Michel <drewmichel1995@gmail.com>
2023-04-04 23:42:37 -0400

    Update headers

--- server/src/main/java/JazzHTTP.java
+++ server/src/main/java/JazzHTTP.java
@@ -236,7 +236,7 @@ public class JazzHTTP {
                 res.status(200);
                 res.type("application/json");
 
-                res.header("Access-Control-Allow-Origin", "https://mbse-rmdev.saic.com:9443");
+                res.header("Access-Control-Allow-Origin", "*");
                 res.header("Access-Control-Allow-Credentials", "true");
                 res.cookie("/", "jazz_rtm_cookie", encodedString, 60*60*24, false, true);
                 return true;
@@ -283,8 +283,8 @@ public class JazzHTTP {
     private static Response getRes(Response res, String type){
         res.status(200);
         res.type(type);
-        //res.header("Access-Control-Allow-Origin", "http://mbse-appld10.corp.saic.com");
-        //res.header("Access-Control-Allow-Origin", "https://mbse-rmdev.saic.com:9443");
+        //res.header("Access-Control-Allow-Origin", "*");
+        //res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Origin", "*");
 
         return res;
